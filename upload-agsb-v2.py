9e3e8d32fbe3739e5cdc174e8db7582192eddf67
cyfurry233 <cyfurry233@gmail.com>
2025-06-09 21:09:42 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -34,7 +34,7 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 USER_NAME = "warma"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 11451                   # Vmess端口，留空或0则自动生成
-DOMAIN = "vps.nu9.xyz"                   # 域名，留空则自动获取
+DOMAIN = "vpn.19991026.xyz"                   # 域名，留空则自动获取
 CF_TOKEN = "eyJhIjoiNzMxNmVjMzIxMzM0MTExODJkMTBiNzYxY2IzOTViYmEiLCJ0IjoiZjEyYTc2MmQtMmZlMC00M2FhLThhNTAtYWNhNDlmMTI4MzllIiwicyI6IlpEVXhNV014T1RRdFpUWTJNQzAwTlRJMUxUbGtPVEV0T1Rsa1l6YzNOakF5Wm1WaCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
